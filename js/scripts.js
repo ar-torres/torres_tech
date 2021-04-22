b2ed86273cfb664905cf4629cc8842d7a17a4aef
Austin Torres <austin.r.torres@gmail.com>
2021-04-21 22:17:14 -0600

    Added Custom PC and HT modals

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(function () {
+	$("#pcQuoteButton").click(function () {
+		$("#pcQuoteModal").modal("show");
+	});
+
+	$("#htConsultButton").click(function () {
+		$("#htConsultModal").modal("show");
+	});
+});
